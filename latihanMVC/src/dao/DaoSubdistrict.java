dc93915d18dfd45c914af3656154c2e7bfa170bc
christinaregita <80805524+christinaregita@users.noreply.github.com>
2021-03-21 23:01:43 +0700

    edit subdistrict

--- latihanMVC/src/dao/DaoSubdistrict.java
+++ latihanMVC/src/dao/DaoSubdistrict.java
@@ -5,10 +5,86 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import koneksi.Koneksi;
+import model.Subdistrict;
+
 /**
  *
  * @author Asus
  */
 public class DaoSubdistrict {
+    
+    Connection connection = Koneksi.getConnection();
+    
+    public boolean insert (Subdistrict subdistrict){
+    PreparedStatement statement;
+    //boolean result = false;
+    try{
+        statement = connection.prepareStatement("INSERT INTO subdistrict (subdistrict_id, subdistrict_name, district_id)VALUES(?,?,?);");
+        statement.setInt(1, subdistrict.getSubdistrictId()); 
+        statement.setString(2, subdistrict.getSubdistrictName()); 
+        statement.setInt(3, subdistrict.getDistrictId());
+        statement.executeUpdate(); //execute
+        statement.close();
+        return true;
+    }catch (SQLException ex){
+        ex.printStackTrace();
+        return false;
+    }
+    }
+    
+    public boolean update (Subdistrict subdistrict){
+    PreparedStatement statement;
+    //boolean result = false;
+    try{
+        statement = connection.prepareStatement("UPDATE subdistrict SET subdistrict_name=? WHERE subdistrict_id=? AND district_id=? ;");
+        statement.setInt(1, subdistrict.getSubdistrictId()); 
+        statement.setString(2, subdistrict.getSubdistrictName()); 
+        statement.setInt(3, subdistrict.getDistrictId());
+        statement.executeUpdate(); //execute
+        statement.close();
+        return true;
+    }catch (SQLException ex){
+        ex.printStackTrace();
+        return false;
+    }
+    }
+    
+    public boolean delete (Subdistrict subdistrict){
+    PreparedStatement statement;
+    //boolean result = false;
+    try{
+        statement = connection.prepareStatement("DELETE FROM subdistrict WHERE subdistrict_id=? ;");
+        statement.setInt(1, subdistrict.getSubdistrictId());
+        statement.executeUpdate(); //execute
+        statement.close();
+        return true;
+    }catch (SQLException ex){
+        ex.printStackTrace();
+        return false;
+    }
+    }
+    
+    public List<Subdistrict> getAll(){
+    List<Subdistrict> listSubdistrict = new ArrayList<Subdistrict>();
+    try{
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM PROVINCE");
+        while (rs.next()){
+            listSubdistrict.add(new Subdistrict(rs.getInt(1), rs.getString(2), rs.getInt(3)));
+        }
+    } catch (SQLException ex){
+        ex.printStackTrace();
+        
+    }
+        return listSubdistrict;
+    }
     
 }
--- latihanMVC/src/model/Subdistrict.java
+++ latihanMVC/src/model/Subdistrict.java
@@ -10,5 +10,45 @@ package model;
  * @author Asus
  */
 public class Subdistrict {
+
+    private int subdistrictId;
+    private String subdistrictName;
+    private int districtId;
+
+    public Subdistrict(int subdistrictId, String subdistrictName, int districtId) {
+        this.subdistrictId = subdistrictId;
+        this.subdistrictName = subdistrictName;
+        this.districtId = districtId;
+    }
+    
+     public Subdistrict() {
+        
+    }
+
+    public int getSubdistrictId() {
+        return subdistrictId;
+    }
+
+    public void setSubdistrictId(int subdistrictId) {
+        this.subdistrictId = subdistrictId;
+    }
+
+    public String getSubdistrictName() {
+        return subdistrictName;
+    }
+
+    public void setSubdistrictName(String subdistrictName) {
+        this.subdistrictName = subdistrictName;
+    }
+
+    public int getDistrictId() {
+        return districtId;
+    }
+
+    public void setDistrictId(int districtId) {
+        this.districtId = districtId;
+    }
+    
+    
     
 }
